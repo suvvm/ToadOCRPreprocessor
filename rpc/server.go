71e6f7bc798e6e42da0a844f9c269733a54264e9
suvvm <suvvm@foxmail.com>
2021-05-17 16:42:41 +0800

    update appInfo

--- rpc/server.go
+++ rpc/server.go
@@ -2,17 +2,22 @@ package rpc
 
 import (
 	"flag"
+	"fmt"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"suvvm.work/ToadOCRPreprocessor/config"
+	"suvvm.work/ToadOCRPreprocessor/dal/db"
 	pb "suvvm.work/ToadOCRPreprocessor/rpc/idl"
 	"syscall"
 	"time"
 )
 
 var (
+	dbConfig = "./conf/db_config.yaml"
 	successMsg = flag.String("pre success msg", "success", "rpc reply message")
 	successCode = flag.Int("pre success code", 0, "rpc reply code")
 	errorCode = flag.Int("pre error code", 1, "rpc reply code")
@@ -23,8 +28,26 @@ var (
 	reg  = flag.String("pre reg", "http://localhost:2379", "register etcd address")
 )
 
+// InitConfig 初始化配置信息
+func InitConfig() {
+	str, err := os.Getwd() // 获取相对路径
+	if err != nil {
+		panic(fmt.Sprintf("filepath failed, err=%v", err))
+	}
+	dbFileName, err := filepath.Abs(filepath.Join(str, dbConfig)) // 获取db配置文件路径
+	if err != nil {
+		panic(fmt.Sprintf("filepath failed, err=%v", err))
+	}
+	conf := config.Init(dbFileName)                    // 读取db配置文件
+	if err = db.InitDB(&conf.DBConfig); err != nil { // 初始化db链接
+		panic(fmt.Sprintf("init db conn err=%v", err))
+	}
+}
+
 func RunRPCServer() {
-	log.Printf("service listen port:%v", port)
+	log.Printf("service init config")
+	InitConfig()
+	log.Printf("service listen port:%v", *port)
 	lis, err := net.Listen("tcp", ":" + *port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
